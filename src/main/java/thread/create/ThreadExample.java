854676da01d285626e7bc2470082ba5e5068726d
panmane18 <pandurang.mane.18@gmail.com>
2025-05-04 16:25:27 +0530

    thread creation using thread class

--- src/main/java/thread/create/ThreadExample.java
+++ src/main/java/thread/create/ThreadExample.java
@@ -1,6 +1,6 @@
 package thread.create;
 
-public class MyThread extends Thread {
+class MyThread extends Thread {
     @Override
     public void run() {
         for (int i = 0; i < 5; i++) {
@@ -14,4 +14,11 @@ public class MyThread extends Thread {
     }
 }
 
-public class 
+public class ThreadExample {
+    public static void main(String[] args) {
+        MyThread thread1 = new MyThread();
+        MyThread thread2 = new MyThread();
+        thread1.start();
+        thread2.start();
+    }
+}
