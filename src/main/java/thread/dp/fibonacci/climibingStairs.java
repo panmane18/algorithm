3b94f9b3e23dff8d8cfa0b9cd65b95c1c833cd54
panmane18 <pandurang.mane.18@gmail.com>
2025-05-11 23:45:31 +0530

    more problem solved

---
+++ src/main/java/thread/dp/fibonacci/climibingStairs.java
@@ -0,0 +1,16 @@
+package thread.dp.fibonacci;
+
+class climibingStairs {
+  public int climbStairs(int n) {
+    int[] dp = new int[n + 1];
+    Arrays.fill(dp, -1);
+    return climbStairs(n, dp);
+  }
+
+  public int climbStairs(int n, int[] dp) {
+    if (n == 0) return 1;
+    if (n == -1) return 0;
+    if (dp[n] != -1) return dp[n];
+    return dp[n] = climbStairs(n - 1,dp) + climbStairs(n - 2,dp);
+  }
+}
